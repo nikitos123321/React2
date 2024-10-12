3e4ca6bf60f757d7185c1f3ecfc20719555c9c7b
nikitos123321 <nikitasaronov1907@gmail.com>
2024-10-12 11:07:12 +0300

    проперделка

--- src/components/CardComponent/Card.jsx
+++ src/components/CardComponent/Card.jsx
@@ -1,10 +1,11 @@
 import style from './Card.module.css'
+import card_img from '/src/assets/Rectangle 3.png'
 
 export function Card() {
     return (
         <>
             <div className={style.card}>
-                <img src="/src/assets/Rectangle 3.png" alt="" />
+                <img src={card_img} alt="" />
                 <p className={style.card_name}>Ароматизатор для автомобиля Diamond candle c ароматом "Кожа и древесина"</p>
                 <div className={style.card_bottom}>
                     <p className={style.card_price}>335 ₽</p>
